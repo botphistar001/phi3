4b23ec28a9b0b419489eb34aa24a6106970b982d
botphistar001 <affiliatecommunity1@gmail.com>
2025-10-14 16:53:18 +0100

    Update token.js

--- token.js
+++ token.js
@@ -1,5 +1,5 @@
 // token.js
 module.exports = {
     telegramToken: '7867524120:AAFH36k_cRPyBHBEZc0UbVi2RSe6DoGxnvQ',
-    prefix: '' // Add prefix to token.js
+    prefix: '/' // Add prefix to token.js
 };
